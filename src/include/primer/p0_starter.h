0ced4545b65e2c276dbb620e6eb6b4fa4fc721e5
pengchang1997 <pengchang1997@126.com>
2021-08-03 10:38:15 +0800

    Project0：C++ Primer

--- src/include/primer/p0_starter.h
+++ src/include/primer/p0_starter.h
@@ -23,7 +23,16 @@ template <typename T>
 class Matrix {
  protected:
   // TODO(P0): Add implementation
-  Matrix(int r, int c) {}
+  Matrix(int r, int c) {
+    // 为矩阵分配内存空间
+    linear = new T[r * c];
+
+    // 设置矩阵行数
+    this->rows = r;
+
+    // 设置矩阵列数
+    this->cols = c;
+  }
 
   // # of rows in the matrix
   int rows;
@@ -51,32 +60,67 @@ class Matrix {
   virtual void MatImport(T *arr) = 0;
 
   // TODO(P0): Add implementation
-  virtual ~Matrix() = default;
+  virtual ~Matrix() {
+    // 释放矩阵所占空间
+    delete[] linear;
+  }
 };
 
 template <typename T>
 class RowMatrix : public Matrix<T> {
  public:
   // TODO(P0): Add implementation
-  RowMatrix(int r, int c) : Matrix<T>(r, c) {}
+  RowMatrix(int r, int c) : Matrix<T>(r, c) {
+    // 为矩阵分配内存空间
+    data_ = new T*[r];
+    for (int i = 0; i < r; i++) {
+      data_[i] = new T[c];
+    }
+  }
 
   // TODO(P0): Add implementation
-  int GetRows() override { return 0; }
+  int GetRows() override {
+    // 返回矩阵行数
+    return this->rows;
+  }
 
   // TODO(P0): Add implementation
-  int GetColumns() override { return 0; }
+  int GetColumns() override {
+    // 返回矩阵列数
+    return this->cols;
+  }
 
   // TODO(P0): Add implementation
-  T GetElem(int i, int j) override { return data_[i][j]; }
+  T GetElem(int i, int j) override {
+    // 获取矩阵指定位置的元素
+    return data_[i][j];
+  }
 
   // TODO(P0): Add implementation
-  void SetElem(int i, int j, T val) override {}
+  void SetElem(int i, int j, T val) override {
+    // 为矩阵的指定位置设置元素值
+    data_[i][j] = val;
+  }
 
   // TODO(P0): Add implementation
-  void MatImport(T *arr) override {}
+  void MatImport(T *arr) override {
+    // 从一维数组向矩阵导入元素
+    for (int i = 0; i < this->rows; i++) {
+      for (int j = 0; j < this->cols; j++) {
+        data_[i][j] = arr[this->cols * i + j];
+      }
+    }
+  }
 
   // TODO(P0): Add implementation
-  ~RowMatrix() override = default;
+  ~RowMatrix() override {
+    // 释放矩阵所占空间
+    for (int i = 0; i < this->rows; i++) {
+      delete[] data_[i];
+    }
+
+    delete[] data_;
+  }
 
  private:
   // 2D array containing the elements of the matrix in row-major format
@@ -94,17 +138,59 @@ class RowMatrixOperations {
   static std::unique_ptr<RowMatrix<T>> AddMatrices(std::unique_ptr<RowMatrix<T>> mat1,
                                                    std::unique_ptr<RowMatrix<T>> mat2) {
     // TODO(P0): Add code
+    if (mat1 == nullptr || mat2 == nullptr) {
+      return std::unique_ptr<RowMatrix<T>>(nullptr);
+    }
 
-    return std::unique_ptr<RowMatrix<T>>(nullptr);
+    if (mat1->GetRows() != mat2->GetRows() || mat1->GetColumns() != mat2->GetColumns()) {
+      return std::unique_ptr<RowMatrix<T>>(nullptr);
+    }
+
+    int rows = mat1->GetRows(), cols = mat1->GetColumns();
+    auto result = std::unique_ptr<RowMatrix<T>>(new RowMatrix<T>(rows, cols));
+    for (int i = 0; i < rows; i++) {
+      for (int j = 0; j < cols; j++) {
+        T val1 = mat1->GetElem(i, j);
+        T val2 = mat2->GetElem(i, j);
+        result->SetElem(i, j, val1 + val2);
+      }
+    }
+
+    return result;
   }
 
   // Compute matrix multiplication (mat1 * mat2) and return the result.
   // Return nullptr if dimensions mismatch for input matrices.
   static std::unique_ptr<RowMatrix<T>> MultiplyMatrices(std::unique_ptr<RowMatrix<T>> mat1,
                                                         std::unique_ptr<RowMatrix<T>> mat2) {
-    // TODO(P0): Add code
+    if (mat1 == nullptr || mat2 == nullptr) {
+      return std::unique_ptr<RowMatrix<T>>(nullptr);
+    }
 
-    return std::unique_ptr<RowMatrix<T>>(nullptr);
+    if (mat1->GetColumns() != mat2->GetRows()) {
+      return std::unique_ptr<RowMatrix<T>>(nullptr);
+    }
+
+    int M = mat1->GetRows(), K = mat2->GetRows(), N = mat2->GetColumns();
+
+    auto result = std::unique_ptr<RowMatrix<T>>(new RowMatrix<T>(M, N));
+
+    // 初始化返回矩阵
+    for (int i = 0; i < M; i++) {
+      for (int j = 0; j < N; j++) {
+        result->SetElem(i, j, 0);
+      }
+    }
+
+    for (int i = 0; i < M; i++) {
+      for (int j = 0; j < N; j++) {
+        for (int k = 0; k < K; k++) {
+          result->SetElem(i, j, result->GetElem(i, j) + mat1->GetElem(i, k) * mat2->GetElem(k, j));
+        }
+      }
+    }
+
+    return result;
   }
 
   // Simplified GEMM (general matrix multiply) operation
@@ -113,8 +199,32 @@ class RowMatrixOperations {
                                                     std::unique_ptr<RowMatrix<T>> matB,
                                                     std::unique_ptr<RowMatrix<T>> matC) {
     // TODO(P0): Add code
+    if (matA == nullptr || matB == nullptr || matC == nullptr) {
+      return std::unique_ptr<RowMatrix<T>>(nullptr);
+    }
 
-    return std::unique_ptr<RowMatrix<T>>(nullptr);
+    int rowsA = matA->GetRows(), colsA = matA->GetColumns(), rowsB = matB->GetRows(), colsB = matB->GetColumns(), rowsC = matC->GetRows(), colsC = matC->GetColumns();
+    if (colsA != rowsB || rowsC != rowsA || colsC != colsB) {
+      return std::unique_ptr<RowMatrix<T>>(nullptr);
+    }
+
+    auto result = std::unique_ptr<RowMatrix<T>>(new RowMatrix<T>(rowsC, colsC));
+
+    // 初始化返回矩阵
+    for (int i = 0; i < rowsC; i++) {
+      for (int j = 0; j < colsC; j++) {
+        result->SetElem(i, j, 0);
+      }
+    }
+
+    for (int i = 0; i < rowsA; i++) {
+      for (int j = 0; j < colsB; j++) {
+        for (int k = 0; k < colsA; k++) {
+          matC->SetElem(i, j, matC->GetElem(i, j) + matA->GetElem(i, k) * matB->GetElem(k, j));
+        }
+      }
+    }
+    return result;
   }
 };
 }  // namespace bustub
